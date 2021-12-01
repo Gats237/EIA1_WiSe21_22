ba623e9c6b1705c1352ac12ceb01ec9cea130fa5
Gats237 <henning.silas.pils@hs-furtwangen.de>
2021-12-01 19:44:26 +0100

    TS Lint aktiviert

--- Task7/tscriptt7.js
+++ Task7/tscriptt7.js
@@ -10,7 +10,7 @@ var samples = ['assets/kick.mp3',
     'assets/laugh-2.mp3'];
 function playSound(audio) {
     var samp = new Audio(audio);
-    samp.play: ();
+    samp.play();
 }
 document.querySelector(".button1").addEventListener("click", function () { playSound(samples[0]); });
 document.querySelector(".button2").addEventListener("click", function () { playSound(samples[1]); });
@@ -21,16 +21,18 @@ document.querySelector(".button6").addEventListener("click", function () { playS
 document.querySelector(".button7").addEventListener("click", function () { playSound(samples[6]); });
 document.querySelector(".button8").addEventListener("click", function () { playSound(samples[7]); });
 document.querySelector(".button9").addEventListener("click", function () { playSound(samples[8]); });
-document.querySelector(".btn").addEventListener("click", mixbeat);
+document.querySelector(".container").addEventListener("click", mixbeat);
 /* Beat*/
-var beat = ['assets/hihat.mp3', 'assets/kick.mp3', 'assets/snare.mp3'];
-var mix = setInterval(mixbeat, 200);
-var index = 0;
+var beat = [new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'), new Audio('assets/snare.mp3')];
 function mixbeat() {
-    var order = new Audio(beat[index]);
-    order.play();
-    index += 1;
-    if (index > beat.length)
-        index = 0, ;
+    setInterval(function () {
+        beat[0].play();
+    }, 300);
+    setInterval(function () {
+        beat[1].play();
+    }, 500);
+    setInterval(function () {
+        beat[2].play();
+    }, 700);
 }
 //# sourceMappingURL=tscriptt7.js.map
